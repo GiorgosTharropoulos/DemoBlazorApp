246af09b348637ac4f13ffd746dc57c01ede3b0c
Kostas Keremis <kkeremis@claster.com>
2021-04-15 16:49:15 +0300

    copypa

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using BlazorApp.Data;
 
 namespace BlazorApp
 {
@@ -26,6 +27,7 @@ namespace BlazorApp
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
+            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
